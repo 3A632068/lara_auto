73bdb8bb0ec1b0b1982674d85ed125efe1321f27
PengHueiSin <eason12326@gmail.com>
2019-10-19 23:28:51 +0800

    測試monolog的使用

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@ require '../vendor/autoload.php';
 use \Demo\HelloWorld as World;
 use Demo\Hello\Lara;
 use Demo\Hello;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 $world = new World();
 $lara= new Lara();
@@ -10,3 +12,12 @@ $vincent= new Hello\Someone('Vincent');
 
 $mary= new \Demo\Hello\Someone('Mary');
 $john= new Demo\Hello\Someone('John');
+
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('path/to/your.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
